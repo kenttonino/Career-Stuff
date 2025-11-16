aac7bf305aeb0359d1efb6724bb287da8b57eb6c
Kent Tonino <tonino.kentlouise@gmail.com>
2025-11-16 14:25:32 +0800

    fix: successful build for opencv

--- opencv-cpp/src/examples/0001_image.cpp
+++ opencv-cpp/src/examples/0001_image.cpp
@@ -9,7 +9,8 @@ using namespace std;
   This example will load an image.
 */
 int ex_0001_load_image(void) {
-  string image_path = cv::samples::findFile("../files/digital_circuit.png");
+  string image_path = "/home/kenttonino/Documents/projects/Career-Stuff/"
+                      "opencv-cpp/src/files/digital_circuit.png";
   cv::Mat img = imread(image_path, cv::IMREAD_COLOR);
 
   if (img.empty()) {
@@ -21,7 +22,7 @@ int ex_0001_load_image(void) {
   int k = cv::waitKey(0); // Wait for a keystroke in the window
 
   if (k == 's') {
-    cv::imwrite("starry_night.png", img);
+    cv::imwrite("../files/digital_circuit_saved.png", img);
   }
 
   return 0;
